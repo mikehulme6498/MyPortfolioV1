67f2877ee884881c6c7e8701524ff5e09299cc0e
Mike Hulme <mikehulme1983@googlemail.com>
2020-11-28 04:39:29 +0000

    updated youtube video links

--- src/Components/Projects/CryptoPic/CryptoPicDemo.js
+++ src/Components/Projects/CryptoPic/CryptoPicDemo.js
@@ -5,7 +5,7 @@ function CryptoPicDemo() {
         
              <div className="demo-video">
                 <div className="section-text"><p>Here is a demo of the software in use </p></div>
-                <iframe width="840" height="472" src="https://www.youtube.com/embed/L8itBSK1z_c" title="crpytopicDemo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="840" height="472" src="https://www.youtube.com/embed/wBzj1Zxue0c" title="crpytopicDemo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         
     )
--- src/Components/Projects/LexLets/LexletsDemo.js
+++ src/Components/Projects/LexLets/LexletsDemo.js
@@ -4,7 +4,7 @@ function LexletsDemo() {
     return (        
             <div className="demo-video">
                 <div className="section-text"><p>Here is a demo of the software in use </p></div>
-                <iframe width="840" height="472" src="https://www.youtube.com/embed/MenOfgBFBYg" title="lexletsDemo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="840" height="472" src="https://www.youtube.com/embed/AAgQ8pNaCwQ" title="lexletsDemo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>        
     )
 }
